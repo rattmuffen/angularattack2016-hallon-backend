40c11003189bbe9144778a24d56f6d350c391f61
Magnus <rattmuffen@gmail.com>
2016-05-14 14:16:21 +0200

    Get twitch streams from gosugamers URL and added ALL filter to get/games
    get dem streams

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
+var https = require('https');
 
 var gosu = require('gosugamers-api');
+var twitchStream = require('twitch-get-stream');
 
 var app = express();
 var http = require('http').Server(app);
@@ -17,6 +19,29 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.set("jsonp callback", true);
+
+
+app.get('/get/stream/:channel', function (req, res) {
+	var channel = req.params.channel;
+	console.log('Get streams for ' + channel);
+	
+	twitchStream.get(channel).then(function (streams) {
+		res.jsonp(JSON.stringify(streams));
+	});
+});
+
+app.get('/get/channel/:url', function (req, res) {
+	var gosuUrl = req.params.url;
+	
+	var tournamentString = gosuUrl.split('/')[5];
+	var tournament = tournamentString.substr(tournamentString.indexOf('-') + 1, tournamentString.length);
+	tournament = tournament.replace(new RegExp('-', 'g'), ' ');
+	console.log('Search for tournament: ' + tournament);
+		
+	callTwitchAPI('search/channels?q=' + encodeURIComponent(tournament), function (results) {
+		res.send(JSON.stringify(results));
+	});
+});
 
 
 app.get('/get/games/:type/:status', function (req, res) {
@@ -51,6 +76,8 @@ app.get('/get/games/:type/:status', function (req, res) {
 				sendMatches = completeMatches;
 			} else if (status == 'upcoming') {
 				sendMatches = upcomingMatches;
+			} else if (status == 'all') {
+				sendMatches = matches;
 			}
 			
 			console.log('Sending ' + sendMatches.length + ' ' + status + ' ' + type + ' matches back.');
@@ -66,9 +93,39 @@ app.get('/get/greeting', function (req, res) {
 });
 
 
-function getDetails(URL, callback) {
+
+function callTwitchAPI(url, callback) {
+	var options = {
+		host: 'api.twitch.tv',
+		port: 443,
+		path: '/kraken/' + url,
+		method: 'GET'
+	};
 	
+	var request = https.request(options, function (resp) {
+		var responseBody = ''
+		resp.setEncoding('utf8');
+
+		resp.on('data', function (chunk) {
+			responseBody += chunk;
+		});
+
+		resp.on('end', function () {
+			request.end();
+			callback(responseBody);
+		});
+
+		resp.on('close', function () {
+			request.end();
+		});
+
+		resp.on('error', function () {
+			request.end();
+		});
+	});
+	request.end();
 }
+
 
 var server = http.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
