310b6567918def12f41ee52568fd8362ce81e393
Magnus <rattmuffen@gmail.com>
2016-05-14 12:39:06 +0200

    Implement gosugamers api
    Get dat data yo

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
 
+var gosu = require('gosugamers-api');
+
 var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 7845;
@@ -17,11 +19,27 @@ app.use(morgan('dev'));
 app.set("jsonp callback", true);
 
 
+app.get('/get/games/:query', function (req, res) {
+	var type = req.params.query;
+	console.log('Got request for ' + type + ' games!');
+	
+	gosu.fetchMatchUrls(type, null, function (error, URLs) {
+		gosu.parseMatches(URLs, function (error, data) {
+			res.jsonp(data);
+		});
+	});
+});
+
 
 app.get('/get/greeting', function (req, res) {
 	var response = {greeting: 'Hej!'};
-	res.jsonp(response);
+	res.jsonp(JSON.stringify(response));
 });
+
+
+function getDetails(URL, callback) {
+	
+}
 
 var server = http.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
